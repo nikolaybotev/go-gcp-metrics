289869056b7bbf0eb10299f33d441642a2ae5510
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-08-13 16:20:12 -0700

    go fmt

--- counter.go
+++ counter.go
@@ -5,34 +5,34 @@ import (
 )
 
 type Counter struct {
-    Name   string
-    Labels map[string]string
-    value  int64
+	Name   string
+	Labels map[string]string
+	value  int64
 }
 
 // NewCounter creates a Counter with the given name and no labels.
 func NewCounter(name string) *Counter {
-    return &Counter{
-        Name:   name,
-        Labels: make(map[string]string),
-    }
+	return &Counter{
+		Name:   name,
+		Labels: make(map[string]string),
+	}
 }
 
 func NewCounterWithLabels(name string, labels map[string]string) *Counter {
-    return &Counter{
-        Name:   name,
-        Labels: labels,
-    }
+	return &Counter{
+		Name:   name,
+		Labels: labels,
+	}
 }
 
 func (c *Counter) Inc() {
-    atomic.AddInt64(&c.value, 1)
+	atomic.AddInt64(&c.value, 1)
 }
 
 func (c *Counter) Add(n int64) {
-    atomic.AddInt64(&c.value, n)
+	atomic.AddInt64(&c.value, n)
 }
 
 func (c *Counter) Value() int64 {
-    return atomic.LoadInt64(&c.value)
+	return atomic.LoadInt64(&c.value)
 }
