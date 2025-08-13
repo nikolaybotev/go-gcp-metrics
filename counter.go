a44f89f3f0502094170b7c5b2277458f22ad4f6b
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-08-13 15:57:37 -0700

    Add Counter class.

---
+++ counter.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"sync/atomic"
+)
+
+type Counter struct {
+    Name   string
+    Labels map[string]string
+    value  int64
+}
+
+// NewCounter creates a Counter with the given name and no labels.
+func NewCounter(name string) *Counter {
+    return &Counter{
+        Name:   name,
+        Labels: make(map[string]string),
+    }
+}
+
+func NewCounterWithLabels(name string, labels map[string]string) *Counter {
+    return &Counter{
+        Name:   name,
+        Labels: labels,
+    }
+}
+
+func (c *Counter) Inc() {
+    atomic.AddInt64(&c.value, 1)
+}
+
+func (c *Counter) Add(n int64) {
+    atomic.AddInt64(&c.value, n)
+}
+
+func (c *Counter) Value() int64 {
+    return atomic.LoadInt64(&c.value)
+}
