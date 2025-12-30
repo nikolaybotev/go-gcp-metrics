e6768eaaf934349ded22f40770aeffa0bb00bd4f
Nikolay Botev <nikolaybotev.b@gmail.com>
2025-12-29 20:16:35 -0800

    Add MetricsEmitter interface.

--- metrics.go
+++ metrics.go
@@ -1,9 +1,19 @@
 package gcpmetrics
 
+import (
+	"context"
+	"time"
+)
+
 // Metrics defines the public interface for metrics implementations.
 type Metrics interface {
 	Counter(name string, labels map[string]string) *Counter
 	Gauge(name string, labels map[string]string) *Gauge
 	Distribution(name, unit string, step, numBuckets int, labels map[string]string) *Distribution
 	AddBeforeEmitListener(listener func())
+}
+
+type MetricsEmitter interface {
+	Emit(ctx context.Context)
+	EmitEvery(ctx context.Context, interval time.Duration) *time.Ticker
 }
